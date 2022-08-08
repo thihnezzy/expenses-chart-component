e9390879936e392ff5289e4eb9bb36e10dfc0b39
Quoc Thinh Nguyen <quoc_thinh.nguyen@insa-cvl.fr>
2022-08-08 19:25:48 +0700

    #3 - Load char from data.json

--- js/app.js
+++ js/app.js
@@ -9,7 +9,44 @@ const getData = async () =>{
         console.error(error);
     }
 }
+
+
 const data = getData();
+
+
+function financial(x) {
+    return Number.parseFloat(x).toFixed(5);
+  }
+
+// const fullBarPercent = 100;
+const fullBarPixel = 150;
+
+
+const modifyDiv = (maxAmount,day) => {
+    const ele = document.querySelector(`.${day['day']}`);
+    ele.setAttribute('value',day['amount']);
+    const height = day['amount'] / maxAmount * fullBarPixel;
+    ele.style.height = `${height}px`;
+    if (financial(day['amount']) == financial(maxAmount)){
+        ele.classList.add('max');
+        ele.style.backgroundColor = "hsl(186,34%,60%)";
+    }
+
+}
+
 data.then((res)=>{
-    console.log(res);
+    const maxAmount = Math.max(...(res.map((obj) =>{
+        return obj['amount'];
+    })));
+    for (let day of res){
+        modifyDiv(maxAmount,day);
+    }
 });
+
+const daySpending = document.querySelectorAll('.data');
+
+for (day of daySpending){
+    day.addEventListener('click', (e) =>{
+        console.log(e);
+    })
+}
